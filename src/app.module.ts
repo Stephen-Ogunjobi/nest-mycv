31cf123c71e581c5cfdec2e123da84c9e2ff94c8
successful <stephenogunjobi400@gmail.com>
2026-02-03 13:07:18 +0100

    app module fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReportsModule } from './reports/reports.module';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { User } from './users/user.entity';
@@ -20,7 +18,7 @@ import { Report } from './reports/report.entity';
     ReportsModule,
     UsersModule,
   ],
-  controllers: [AppController, UsersController],
-  providers: [AppService, UsersService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
-  await app.listen(process.env.PORT ?? 5001);
+  await app.listen(process.env.PORT ?? 3002);
 }
 bootstrap();
