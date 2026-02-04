9d12c0f2f119343ed4fad3653143f7d7b72758c1
successful <stephenogunjobi400@gmail.com>
2026-02-04 10:56:29 +0100

    wip: serialize typing

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -4,22 +4,22 @@ import {
   NestInterceptor,
   UseInterceptors,
 } from '@nestjs/common';
-import { plainToInstance } from 'class-transformer';
+import { ClassConstructor, plainToInstance } from 'class-transformer';
 import { map, Observable } from 'rxjs';
 
-export function Serialize(dto: any) {
+export function Serialize<T>(dto: ClassConstructor<T>) {
   return UseInterceptors(new SerializeInterceptor(dto));
 }
 
-export class SerializeInterceptor implements NestInterceptor {
-  constructor(private dto: any) {}
+export class SerializeInterceptor<T> implements NestInterceptor {
+  constructor(private dto: ClassConstructor<T>) {}
 
   intercept(
     context: ExecutionContext,
-    handler: CallHandler<any>,
-  ): Observable<any> | Promise<Observable<any>> {
+    handler: CallHandler,
+  ): Observable<T> | Promise<Observable<T>> {
     return handler.handle().pipe(
-      map((data: any) => {
+      map((data: unknown) => {
         return plainToInstance(this.dto, data, {
           excludeExtraneousValues: true,
         });
