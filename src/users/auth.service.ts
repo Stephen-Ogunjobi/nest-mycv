9afac6f7b3df5c9a483909f8d4ac8811c222f86f
successful <stephenogunjobi400@gmail.com>
2026-03-01 15:41:27 +0100

    wip: update

---
+++ src/users/auth.service.ts
@@ -0,0 +1,39 @@
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(private userService: UsersService) {}
+
+  async signup(email: string, password: string) {
+    const users = await this.userService.find(email);
+    if (users.length) {
+      throw new BadRequestException('email already registered');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await this.userService.create(email, hashedPassword);
+
+    return user;
+  }
+
+  async signin(email: string, password) {
+    const [user] = await this.userService.find(email);
+
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) {
+      throw new UnauthorizedException('Invalid Credentials');
+    }
+  }
+}
